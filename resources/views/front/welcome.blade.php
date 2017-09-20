6517af77a83413e50f80c59717c16799bc886e82
Nathan Connor <ninjawite@gmail.com>
2017-09-20 13:04:39 +0100

    added a welcome back message the top, altered the margins ont top and bottom to be less

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -14,6 +14,28 @@
         <div class="col-md-8 col-md-offset-2">
             @if(count($comments))
                 <div class="infinite-scroll">
+                    <div class="post-container container-fluid mb-25 blue-border">
+                        <div class="col-md-12 comment-top">
+                            <div class="col-md-2 pl-0 pr-0">
+                                <i class="fa fa-4x fa-hand-o-right text-primary pt-5 pb-5 pl-15"></i>
+                            </div>
+                            <div class="col-md-10 pl-0">
+                                <div class="profile-desc">
+                                    <a class="profile-link">
+                                        Hey {{ Auth()->user()->name }}!
+                                    </a>
+                                </div>
+                                <div class="profile-extra">
+                                    <p>Welcome back.<br>
+                                        We've added a few new things to your feed, take a look!</p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div class="blur"></div>
+                    </div>
+
                     @include('front.posts.posts')
                 </div>
             @endif
