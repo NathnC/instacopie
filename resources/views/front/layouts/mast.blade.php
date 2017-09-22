576d028302fdcae708b7180a330cd437a46af0fa
Nathan <ninjawite@gmail.com>
2017-09-22 22:58:58 +0100

    moved mast stuff into the middle

--- resources/views/front/layouts/mast.blade.php
+++ resources/views/front/layouts/mast.blade.php
@@ -1,6 +1,5 @@
 <nav class="navbar navbar-default">
     <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
@@ -10,15 +9,17 @@
             </button>
             <a class="navbar-brand nice-font" href="#">Instacopie</a>
         </div>
+        <div class="col-md-8 col-md-offset-1">
+        <!-- Brand and toggle get grouped for better mobile display -->
+
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse padding-nos" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <form class="navbar-form navbar-left">
                     <div class="form-group">
-                        <input type="text" class="form-control no-rounding" placeholder="Search">
+                        <input type="text" class="form-control search-box" placeholder="Search">
                     </div>
-                    <button type="submit" class="btn btn-default no-rounding"><i class="fa fa-search"></i></button>
                 </form>
             </ul>
             <ul class="nav navbar-nav navbar-right">
@@ -45,5 +46,6 @@
 
             </ul>
         </div><!-- /.navbar-collapse -->
+        </div>
     </div><!-- /.container-fluid -->
 </nav>
